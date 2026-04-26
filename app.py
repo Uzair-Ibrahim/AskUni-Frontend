0f664fc01fc8654988c6b187c02c200156426e37
Haseeb Lodhi <haseeblodhi1899@gmail.com>
2026-04-26 22:54:44 +0500

    Change AskUni chatbot frontend

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-# run: chainlit run app.py -w
+# to run: chainlit run app.py -w
 
 import chainlit as cl
+import asyncio
+
+
+async def get_llm_answer(user_input: str) -> str:
+
+    # TODO: Replace this with a real API call to your FastAPI backend
+
+    # 1 second delay to test frontend
+    await asyncio.sleep(2)
+    
+    # fake response for testing
+    return f"[Backend] You said: '{user_input}'"
+
 
 @cl.on_message
 async def process_user_message(message: cl.Message):
-    reply_text = f"You said: {message.content}"
     
-    await cl.Message(content=reply_text).send()  
+    llm_message = cl.Message(content="Thinking...")
+    await llm_message.send()
+    
+    final_reply = await get_llm_answer(message.content)
+    
+    llm_message.content = final_reply
+    await llm_message.update()
